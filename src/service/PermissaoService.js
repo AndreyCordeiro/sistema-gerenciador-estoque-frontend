ea2a1319f73d5c979a11d8d5a2331903f0f2d764
Andrey <andreybr2002@hotmail.com>
2022-11-18 21:46:30 -0300

    Implementado crud de Permissão

--- src/service/PermissaoService.js
+++ src/service/PermissaoService.js
@@ -1,2 +1,21 @@
+import axios from "axios";
+
 export class PermissaoService {
+    url = 'http://localhost:8080/api/permissao';
+
+    listarTodos() {
+        return axios.get(this.url);
+    }
+
+    inserir(objeto) {
+        return axios.post(this.url + '/cadastrar', objeto);
+    }
+
+    alterar(objeto) {
+        return axios.put(this.url + '/atualizar/' + objeto.id, objeto);
+    }
+
+    excluir(id) {
+        return axios.delete(this.url + '/deletar/' + id);
+    }
 }
