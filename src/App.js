ef6ef6021e4f8c96daac67e8bdc63a801061dbcf
Andrey <andreybr2002@hotmail.com>
2022-11-29 23:28:24 -0300

    Implementado tela de Usuários

--- src/App.js
+++ src/App.js
@@ -9,29 +9,6 @@ import {AppMenu} from './AppMenu';
 import {AppConfig} from './AppConfig';
 
 import Dashboard from './components/Dashboard';
-import ButtonDemo from './components/ButtonDemo';
-import ChartDemo from './components/ChartDemo';
-import Documentation from './components/Documentation';
-import FileDemo from './components/FileDemo';
-import FloatLabelDemo from './components/FloatLabelDemo';
-import FormLayoutDemo from './components/FormLayoutDemo';
-import InputDemo from './components/InputDemo';
-import ListDemo from './components/ListDemo';
-import MenuDemo from './components/MenuDemo';
-import MessagesDemo from './components/MessagesDemo';
-import MiscDemo from './components/MiscDemo';
-import OverlayDemo from './components/OverlayDemo';
-import MediaDemo from './components/MediaDemo';
-import PanelDemo from './components/PanelDemo';
-import TableDemo from './components/TableDemo';
-import TreeDemo from './components/TreeDemo';
-import InvalidStateDemo from './components/InvalidStateDemo';
-import BlocksDemo from './components/BlocksDemo';
-import IconsDemo from './components/IconsDemo';
-
-import Crud from './pages/Crud';
-import EmptyPage from './pages/EmptyPage';
-import TimelineDemo from './pages/TimelineDemo';
 
 import PrimeReact from 'primereact/api';
 import {Tooltip} from 'primereact/tooltip';
@@ -50,6 +27,7 @@ import './assets/demo/flags/flags.css';
 import './assets/demo/Demos.scss';
 import './assets/layout/layout.scss';
 import './App.scss';
+import Usuario from "./pages/Usuario";
 
 
 const App = () => {
@@ -181,108 +159,10 @@ const App = () => {
                 },
                 {
                     label: 'Permissões', icon: 'pi pi-lock-open', to: '/permissoes'
-                }]
-        },
-        {
-            label: 'UI Components', icon: 'pi pi-fw pi-sitemap',
-            items: [
-                {label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/formlayout'},
-                {label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/input'},
-                {label: "Float Label", icon: "pi pi-fw pi-bookmark", to: "/floatlabel"},
-                {label: "Invalid State", icon: "pi pi-fw pi-exclamation-circle", to: "invalidstate"},
-                {label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/button'},
-                {label: 'Table', icon: 'pi pi-fw pi-table', to: '/table'},
-                {label: 'List', icon: 'pi pi-fw pi-list', to: '/list'},
-                {label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/tree'},
-                {label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/panel'},
-                {label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/overlay'},
-                {label: "Media", icon: "pi pi-fw pi-image", to: "/media"},
-                {label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/menu'},
-                {label: 'Message', icon: 'pi pi-fw pi-comment', to: '/messages'},
-                {label: 'File', icon: 'pi pi-fw pi-file', to: '/file'},
-                {label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/chart'},
-                {label: 'Misc', icon: 'pi pi-fw pi-circle-off', to: '/misc'},
-            ]
-        },
-        {
-            label: 'UI Blocks',
-            items: [
-                {label: 'Free Blocks', icon: 'pi pi-fw pi-eye', to: '/blocks', badge: "NEW"},
-                {label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: 'https://www.primefaces.org/primeblocks-react'}
-            ]
-        },
-        {
-            label: 'Icons',
-            items: [
-                {label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/icons'}
-            ]
-        },
-        {
-            label: 'Pages', icon: 'pi pi-fw pi-clone',
-            items: [
-                {label: 'Crud', icon: 'pi pi-fw pi-user-edit', to: '/crud'},
-                {label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/timeline'},
-                {label: 'Empty', icon: 'pi pi-fw pi-circle-off', to: '/empty'}
-            ]
-        },
-        {
-            label: 'Menu Hierarchy', icon: 'pi pi-fw pi-search',
-            items: [
-                {
-                    label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
-                    items: [
-                        {
-                            label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark'},
-                            ]
-                        },
-                        {
-                            label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 1.2.2', icon: 'pi pi-fw pi-bookmark'}
-                            ]
-                        },
-                    ]
                 },
                 {
-                    label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
-                    items: [
-                        {
-                            label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 2.1.3', icon: 'pi pi-fw pi-bookmark'},
-                            ]
-                        },
-                        {
-                            label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark'},
-                                {label: 'Submenu 2.2.2', icon: 'pi pi-fw pi-bookmark'}
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Get Started',
-            items: [
-                {
-                    label: 'Documentation', icon: 'pi pi-fw pi-question', command: () => {
-                        window.location = "#/documentation"
-                    }
+                    label: 'Usuários', icon: 'pi pi-users', to: '/usuarios'
                 },
-                {
-                    label: 'View Source', icon: 'pi pi-fw pi-search', command: () => {
-                        window.location = "https://github.com/primefaces/sakai-react"
-                    }
-                }
             ]
         }
     ];
@@ -326,32 +206,11 @@ const App = () => {
             <div className="layout-main-container">
                 <div className="layout-main">
                     <Route path="/" exact render={() => <Dashboard colorMode={layoutColorMode} location={location}/>}/>
-                    <Route path="/formlayout" component={FormLayoutDemo}/>
-                    <Route path="/input" component={InputDemo}/>
-                    <Route path="/floatlabel" component={FloatLabelDemo}/>
-                    <Route path="/invalidstate" component={InvalidStateDemo}/>
-                    <Route path="/button" component={ButtonDemo}/>
-                    <Route path="/table" component={TableDemo}/>
-                    <Route path="/list" component={ListDemo}/>
-                    <Route path="/tree" component={TreeDemo}/>
-                    <Route path="/panel" component={PanelDemo}/>
-                    <Route path="/overlay" component={OverlayDemo}/>
-                    <Route path="/media" component={MediaDemo}/>
-                    <Route path="/menu" component={MenuDemo}/>
-                    <Route path="/messages" component={MessagesDemo}/>
-                    <Route path="/blocks" component={BlocksDemo}/>
-                    <Route path="/icons" component={IconsDemo}/>
-                    <Route path="/file" component={FileDemo}/>
-                    <Route path="/chart" render={() => <ChartDemo colorMode={layoutColorMode} location={location}/>}/>
-                    <Route path="/misc" component={MiscDemo}/>
-                    <Route path="/timeline" component={TimelineDemo}/>
-                    <Route path="/crud" component={Crud}/>
-                    <Route path="/empty" component={EmptyPage}/>
-                    <Route path="/documentation" component={Documentation}/>
                     <Route path="/fabricantes" component={Fabricante}/>
                     <Route path="/categorias" component={Categoria}/>
                     <Route path="/produtos" component={Produto}/>
                     <Route path="/permissoes" component={Permissao}/>
+                    <Route path="/usuarios" component={Usuario}/>
                 </div>
 
                 <AppFooter layoutColorMode={layoutColorMode}/>
